68aaaa530e37b426ab0528562bd85f05ce80fc5b
OneOfSome-Lab <58184282+OneOfSome-Lab@users.noreply.github.com>
2020-01-15 16:54:21 +0100

    Create kcesarhack.py

---
+++ kcesarhack.py
@@ -0,0 +1,19 @@
+#Cracker. 
+#This srcipt decrypts what was encrypted with kcesar.py also returns the encryption key
+#Python3.6.9 blackfriar@gmx.es 
+
+ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,;:()%&$!?€'
+
+kriptogram = input ('Introduce your Kriptogram: ')
+
+for x in range (1, len(ABC)):
+	ex = ' '
+	for b in kriptogram:
+		if b in ABC:
+			pos = ABC.find(b)
+			#crack
+			pos = (pos - x) % len (ABC)
+			ex += ABC[pos]
+		else:
+			ex += b
+	print ('Key %d: %s' % (x, ex))
